7935fe2ea2a68fcf444951d4fda9c546cf6a589a
Kiril Yanev <kl_yanev@abv.bg>
2024-03-04 22:28:09 +0200

    fix: implement player movement

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ class Player {
 
     update() {
         this.draw();
+        this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
         if (this.position.y + this.height + this.velocity.y <= canvas.height)
@@ -36,13 +37,66 @@ class Player {
 }
 
 const player = new Player();
-player.update();
+const keys = {
+    right: {
+        pressed: false,
+    },
+    left: {
+        pressed: false,
+    }
+}
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
+
+    if (keys.right.pressed) {
+        player.velocity.x = 5;
+    } else if (keys.left.pressed) {
+        player.velocity.x = -5;
+    } else player.velocity.x = 0
 }
 
 
 animate();
+
+addEventListener('keydown', ({ key }) => {
+    switch (key) {
+        case 'a':
+            console.log('left');
+            keys.left.pressed = true;
+            break;
+        case 's':
+            console.log('down');
+            break;
+        case 'd':
+            console.log('right');
+            keys.right.pressed = true;
+            break;
+        case 'w':
+            console.log('up');
+            player.velocity.y -= 20;
+            break;
+    }
+});
+
+addEventListener('keyup', ({ key }) => {
+    switch (key) {
+        case 'a':
+            console.log('left');
+            keys.left.pressed = false;
+            break;
+        case 's':
+            console.log('down');
+            break;
+        case 'd':
+            console.log('right');
+            keys.right.pressed = false;
+            break;
+        case 'w':
+            console.log('up');
+            player.velocity.y -= 20;
+            break;
+    }
+});
