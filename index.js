b929fb9fa6a324703933f8ff7a8f95ad2fe9e3a4
Kiril Yanev <kl_yanev@abv.bg>
2024-03-06 18:10:02 +0200

    fix: add win scenario

--- index.js
+++ index.js
@@ -62,6 +62,8 @@ const keys = {
     }
 }
 
+let scrollOffset = 0;
+
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
@@ -78,15 +80,15 @@ function animate() {
         player.velocity.x = 0;
 
         if (keys.right.pressed) {
+            scrollOffset += 5;
             platforms.forEach(platform => {
                 platform.position.x -= 5;
             })
-
         } else if (keys.left.pressed) {
+            scrollOffset -= 5;
             platforms.forEach(platform => {
                 platform.position.x += 5;
             })
-
         }
     }
 
@@ -99,6 +101,10 @@ function animate() {
             player.velocity.y = 0;
         }
     })
+
+    if (scrollOffset > 2000) {
+        console.log('You win!');
+    }
 }
 
 
